7eba51ee020a2039cb8b56cabac4e87c13d28dd4
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-02-04 07:41:11 +0600

    Create CategoryRow.jsx

---
+++ src/components/CategoryRow.jsx
@@ -0,0 +1,26 @@
+import { memo } from "react";
+import { useNavigate } from "react-router-dom";
+
+function CategoryRow({ cats }) {
+  const nav = useNavigate();
+
+  return (
+    <div className="catRow">
+      {cats.map((c) => (
+        <button
+          key={c._id}   // ❗ index না, শুধু _id
+          className="catItem"
+          type="button"
+          onClick={() => nav(`/shop?category=${c._id}`)}
+        >
+          <div className="catIcon">
+            <img src={c.image} alt={c.name} />
+          </div>
+          <div className="catName">{c.name}</div>
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default memo(CategoryRow); // 🔒 re-render lock
