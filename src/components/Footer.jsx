3a0666392d207280363d3618f4afd4e2a2b72a4c
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-02-07 17:49:29 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import "./Footer.css";
+import "../styles/footer.css";
 import logo from "../assets/logo.png";
 
 export default function Footer() {
