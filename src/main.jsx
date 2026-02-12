ee26a6b7f17ed8fb4b6e2c5df5123e647697e8f0
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-02-12 23:29:15 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,13 @@ import "./styles/auth.css";
 import { CartProvider } from "./context/CartContext";
 import { FavoritesProvider } from "./context/FavoritesContext";
 import { AuthProvider } from "./context/AuthContext";
+import { HelmetProvider } from "react-helmet-async";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+     <HelmetProvider>
       <CartProvider>
         <FavoritesProvider>
           <AuthProvider>
@@ -20,6 +23,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           </AuthProvider>
         </FavoritesProvider>
       </CartProvider>
+     </HelmetProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
